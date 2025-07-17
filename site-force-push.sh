93154cabb63323cdfe085786f3082efda187b399
jason-laumeister <jlaumeister@gmail.com>
2025-07-16 21:38:32 -0700

    init Hugo site and add ananke theme

---
+++ site-force-push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+echo 'Force commit and push to rebuild github pages by updating the hash...'
+
+echo 'DISABLED - commented out code below to prevent accidental pushes'
+
+# git commit -m 'force rebuild of github pages' --allow-empty
+# git push origin master
